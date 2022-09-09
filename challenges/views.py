aa5845891b9eb753e462a1c3bedb780d07a9d1c7
Zohaib Khan <zohaibk1111@gmail.com>
2022-09-08 23:40:43 -0400

    Added a home page for the month challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from types import NoneType
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -16,11 +17,22 @@ monthly_challenges = {
     'september': 'Learn Django for 20 minutes7',
     'october': 'Learn Django for 20 minutes8',
     'november': 'Learn Django for 20 minutes9',
-    'december': 'Learn Django for 20 minutes10',
-
-    
+    'december': 'Learn Django for 20 minutes10', 
     
 }
+
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        new_path = reverse('month-challenge', args=[month])
+        list_items += f"<li><a href=\"{new_path}\">{capitalized_month}</a></li>"
+
+    return_val = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(return_val)
 
 def monthly_challenge(request, month):
     # view_text = None
@@ -37,10 +49,11 @@ def monthly_challenge(request, month):
     # return HttpResponse(view_text)
     try:
         challenge_text = monthly_challenges[month]
+        response_text = f"<h1>{challenge_text}</h1>"
     except:
-        return HttpResponseNotFound("This month is not accepted")
+        return HttpResponseNotFound("<h1>This month is not accepted</h1>")
     
-    return HttpResponse(challenge_text)
+    return HttpResponse(response_text)
 
 
 
@@ -50,4 +63,5 @@ def monthly_challenge_by_number(request, month):
     if month > len(months):
         return HttpResponseNotFound("invalid month")
     forward_month = months[month - 1]
-    return HttpResponseRedirect(f'/challenges/{forward_month}')
+    redirect_path = reverse('month-challenge', args=[forward_month]) # /challenge/janaury
+    return HttpResponseRedirect(redirect_path)
